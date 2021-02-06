e3b8ba1f0624ad5db08b7b3d8ab70ac34e560af8
favo004 <jfavo@hotmail.com>
2021-02-06 13:41:50 -0500

    Created the basic sprite along with the cursor and map cells

---
+++ PathfindingApp/Sprites/Cell.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PathfindingApp.Sprites
+{
+    public class Cell : Sprite
+    {
+        public bool Highlighted;
+        public Color HighlightColor;
+
+        private readonly string _textureKey;
+        private readonly Vector2 _mapPosition;
+
+        private Effect flashEffect;
+        private Effect outlineEffect;
+
+        /// <summary>
+        /// Sprite that represents map graphics
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="sourceRect"></param>
+        /// <param name="position"></param>
+        /// <param name="key"></param>
+        public Cell(Vector2 position, Vector2 mapPosition, string textureKey)
+            : base(position)
+        {
+            _mapPosition = mapPosition;
+            _textureKey = textureKey;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            _texture = content.Load<Texture2D>("Graphics/Assets");
+
+            switch (_textureKey)
+            {
+                case "wall":
+                    _sourceRect = new Rectangle(16, 0, 16, 16);
+                    break;
+                case "floor":
+                    _sourceRect = new Rectangle(0, 16, 16, 16);
+                    break;
+                default:
+                    _sourceRect = new Rectangle(0, 16, 16, 16);
+                    break;
+            }
+            flashEffect = content.Load<Effect>("Shaders/FlashToWhite");
+            outlineEffect = content.Load<Effect>("Shaders/Outline");
+            outlineEffect.Parameters["texelSize"].SetValue(new Vector2(1f / (_sourceRect.Width - 1f), 1f / (_sourceRect.Height - 1f)));
+            outlineEffect.Parameters["outlineColor"].SetValue(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+
+            _origin = new Vector2(_sourceRect.Width / 2, _sourceRect.Height / 2);
+            Bounds = new Rectangle(_position.ToPoint() + _mapPosition.ToPoint(), new Point(_sourceRect.Width, _sourceRect.Height));
+        }
+
+        /// <summary>
+        /// Highlights cell
+        /// </summary>
+        public void Highlight()
+        {
+            if (_textureKey != "floor") return;
+            if (!Highlighted)
+            {
+                Highlighted = true;
+                //_color = Color.Yellow;
+                _effect = flashEffect;
+            }
+        }
+        public void UnHighlight()
+        {
+            if (Highlighted)
+            {
+                Highlighted = false;
+                //_color = Color.White;
+                _effect = null;
+            }
+        }
+    }
+}
---
+++ PathfindingApp/Sprites/Cursor.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace PathfindingApp.Sprites
+{
+    public class Cursor : Sprite
+    {
+        public Rectangle ClickBounds
+        {
+            get => new Rectangle(_position.ToPoint() + clickBoundsPositionOffset, _clickDimensions);
+        }
+
+        private Point clickBoundsPositionOffset = new Point(7, 0);
+        private Point _clickDimensions = new Point(1, 1);
+
+        private Dictionary<string, Rectangle> _cursorRects = new Dictionary<string, Rectangle>
+        {
+            { "idle", new Rectangle(0, 0, 32, 32) },
+            { "click", new Rectangle(0, 32, 32, 32) }
+        };
+
+        private MouseState _previousMouseState;
+        private bool _clicked;
+
+        public Cursor()
+        {          
+            _color = Color.White;
+            _scale = Vector2.One;
+
+            _previousMouseState = Mouse.GetState();
+        }
+
+        public void SetPosition(Vector2 newPos)
+        {
+            _position = newPos;
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            _texture = content.Load<Texture2D>("Graphics/Cursor");
+
+            _sourceRect = _cursorRects["idle"];
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            CheckForInput();
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Checks to see if cursor has clicked on the bounds
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <returns></returns>
+        public bool HasClickOn(Rectangle bounds)
+        {
+            return _clicked && ClickBounds.Intersects(bounds);
+        }
+
+        private void CheckForInput()
+        {
+            MouseState mouseState = Mouse.GetState();
+            SetPosition(mouseState.Position.ToVector2());
+
+            if(mouseState.LeftButton == ButtonState.Pressed &&
+               _previousMouseState.LeftButton == ButtonState.Pressed)
+            {
+                _sourceRect = _cursorRects["click"];
+                _clicked = true;
+            }
+            else
+            {
+                _sourceRect = _cursorRects["idle"];
+                _clicked = false;
+            }
+
+            _previousMouseState = mouseState;
+        }
+    }
+}
